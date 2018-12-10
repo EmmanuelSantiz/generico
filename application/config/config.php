7676406c88d469bb3532d22c7b6a963f6c6b3990
EmmanuelSantiz <emmanuel.07.01@hotmail.com>
2018-12-10 10:42:37 -0600

    actualizacion de configuracion inicial

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-
+//$config['base_url'] = '';
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' : 'http://';
+$config['base_url'] = $protocol.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 /*
 |--------------------------------------------------------------------------
 | Index File
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 	
 	public function index() {
+		/*if($this->session->userdata('bit_sesion')) {
+			echo 'Con Sesion';
+		} else {
+			echo 'Sin Sesion';
+		}*/
 		$this->load->view('welcome_message');
 	}
 }
